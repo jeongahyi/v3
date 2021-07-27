041e4d8d3b71a59820876947b66c2c2473bddfce
Jeongah Yi <jeongahyi@gmail.com>
2021-07-27 16:55:22 +0900

    Add dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,3 +108,31 @@ function scrollUp(){
   }
 }
 window.addEventListener('scroll', scrollUp)
+
+// DARK LIGHT THEME
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// Obtain the current theme that the interface has by vaqlidating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark': 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+if(selectedTheme){
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  // Save the theme and the  current icon that the user choose
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
